e284c91eca58d58dd8a2970a7ea4c9554f50ead4
SimoneG30 <simone.gaspo@gmail.com>
2021-01-31 14:04:28 +0100

    update

--- gestionearticolijspservletjpamaven/src/main/java/it/gestionearticolijspservletjpamaven/web/servlet/ExecuteUpdateArticoloServlet.java
+++ gestionearticolijspservletjpamaven/src/main/java/it/gestionearticolijspservletjpamaven/web/servlet/ExecuteUpdateArticoloServlet.java
@@ -1,41 +1,84 @@
 package it.gestionearticolijspservletjpamaven.web.servlet;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ExecuteUpdateArticoloServlet
- */
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import it.gestionearticolijspservletjpamaven.model.Articolo;
+import it.gestionearticolijspservletjpamaven.service.MyServiceFactory;
+
 @WebServlet("/ExecuteUpdateArticoloServlet")
 public class ExecuteUpdateArticoloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ExecuteUpdateArticoloServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String idArticoloDaModificare = request.getParameter("idArticolo");
+		Long idArticoloDaModificareParsato = Long.parseLong(idArticoloDaModificare);
+		String codiceInputParam = request.getParameter("codice");
+		String descrizioneInputParam = request.getParameter("descrizione");
+		String prezzoInputStringParam = request.getParameter("prezzo");
+		Integer prezzoParsato = Integer.parseInt(prezzoInputStringParam);
+		String dataArrivoStringParam = request.getParameter("dataArrivo");
+		Date dataArrivoParsed = parseDateArrivoFromString(dataArrivoStringParam);
+
+		if (!this.validateInput(codiceInputParam, descrizioneInputParam, prezzoInputStringParam, dataArrivoStringParam)
+				&& dataArrivoParsed == null) {
+			request.setAttribute("errorMessage", "Attenzione sono presenti errori di validazione");
+			request.getRequestDispatcher("/articolo/edit.jsp").forward(request, response);
+			return;
+		}
+
+		try {
+			Articolo articoloInstance = MyServiceFactory.getArticoloServiceInstance().caricaSingoloElemento(idArticoloDaModificareParsato);
+			articoloInstance.setCodice(codiceInputParam);
+			articoloInstance.setDescrizione(descrizioneInputParam);
+			articoloInstance.setPrezzo(prezzoParsato);
+			articoloInstance.setDataArrivo(dataArrivoParsed);
+			MyServiceFactory.getArticoloServiceInstance().aggiorna(articoloInstance);
+			request.setAttribute("listaArticoliAttribute", MyServiceFactory.getArticoloServiceInstance().listAll());
+			request.setAttribute("successMessage", "Operazione effettuata con successo");
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("errorMessage", "Attenzione si è verificato un errore.");
+			request.getRequestDispatcher("/articolo/edit.jsp").forward(request, response);
+			return;
+		}
+
+		// andiamo ai risultati
+		request.getRequestDispatcher("/articolo/results.jsp").forward(request, response);
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	private boolean validateInput(String codiceInputParam, String descrizioneInputParam, String prezzoInputStringParam,
+			String dataArrivoStringParam) {
+		// prima controlliamo che non siano vuoti
+		if (StringUtils.isBlank(codiceInputParam) && StringUtils.isBlank(descrizioneInputParam)
+				&& !NumberUtils.isCreatable(prezzoInputStringParam) && StringUtils.isBlank(dataArrivoStringParam)) {
+			return false;
+		}
+		return true;
 	}
 
+	private Date parseDateArrivoFromString(String dataArrivoStringParam) {
+		if (StringUtils.isBlank(dataArrivoStringParam))
+			return null;
+
+		try {
+			return new SimpleDateFormat("yyyy-MM-dd").parse(dataArrivoStringParam);
+		} catch (ParseException e) {
+			return null;
+		}
+	}
 }
--- gestionearticolijspservletjpamaven/src/main/java/it/gestionearticolijspservletjpamaven/web/servlet/PrepareUpdateArticoloServlet.java
+++ gestionearticolijspservletjpamaven/src/main/java/it/gestionearticolijspservletjpamaven/web/servlet/PrepareUpdateArticoloServlet.java
@@ -7,15 +7,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import it.gestionearticolijspservletjpamaven.service.MyServiceFactory;
+
 
 @WebServlet("/PrepareUpdateArticoloServlet")
 public class PrepareUpdateArticoloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+    	Long idDaModificare = Long.parseLong(request.getParameter("idArticolo"));
+    	
+		try {
+			request.setAttribute("articoloDaModificare", MyServiceFactory.getArticoloServiceInstance().caricaSingoloElemento(idDaModificare));
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("errorMessage", "Attenzione si è verificato un errore.");
+			request.getRequestDispatcher("/articolo/result.jsp").forward(request, response);
+			return;		
+			}
 
-		request.getRequestDispatcher("/articolo/update.jsp").forward(request, response);
+		request.getRequestDispatcher("/articolo/edit.jsp").forward(request, response);
 
 	}
 
